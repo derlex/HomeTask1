981fb48effb5a16e60bdb84f0d5393760e1ec09c
Oleksandr Petukhov <oleksandr.petukhov@gmail.com>
2018-06-13 17:05:15 +0300

    HomeTask1 | Created array with first ten odd numbers; added test

--- src/main/java/ArrayMadness.java
+++ src/main/java/ArrayMadness.java
@@ -1,2 +1,12 @@
 public class ArrayMadness {
+    public static int[] createArrayWithFirstTenOddNumbers() {
+        int[] array = new int[10];
+        int x = 1;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = x;
+            x = x + 2;
+        }
+
+        return array;
+    }
 }
