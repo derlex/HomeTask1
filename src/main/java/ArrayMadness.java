23a1ed5814ffe15e5355688c0bb035f9cbda964b
Oleksandr Petukhov <oleksandr.petukhov@gmail.com>
2018-06-13 18:24:32 +0300

    HomeTask1 | Created array and replaced maximum and minimum elements in it; added test

--- src/main/java/ArrayMadness.java
+++ src/main/java/ArrayMadness.java
@@ -9,4 +9,25 @@ public class ArrayMadness {
 
         return array;
     }
+
+    public static int[] replaceMaxAndMinElementsInArray(int[] array) {
+        int max = array[0];
+        int min = array[0];
+        int elementMax = 0;
+        int elementMin = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+                elementMax = i;
+            }
+            if (min > array[i]) {
+                min = array[i];
+                elementMin = i;
+            }
+        }
+        array[elementMin] = max;
+        array[elementMax] = min;
+
+        return array;
+    }
 }
--- src/test/java/ArrayMadnessTest.java
+++ src/test/java/ArrayMadnessTest.java
@@ -1,2 +1,22 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 public class ArrayMadnessTest {
+    @Test
+    public void createArrayWithFirstTenOddNumbersTest() throws Exception {
+        int[] array = ArrayMadness.createArrayWithFirstTenOddNumbers();
+        assertEquals(true, Arrays.equals(array, new int[]{1, 3, 5, 7, 9, 11, 13, 15, 17, 19}));
+    }
+
+    @Test
+    public void replaceMaxAndMinElementsInArrayTest() throws Exception {
+        int[] actualArray = new int[]{3,-7,0,6,9};
+        int[] expectedArray = new int[]{3,9,0,6,-7};
+        int[] replacedArray = ArrayMadness.replaceMaxAndMinElementsInArray(actualArray);
+        assertArrayEquals(expectedArray, replacedArray);
+    }
 }
